a6dcda8db6022c304498e3af7df61729bfb854ec
LaMeduseO <vincepro59@gmail.com>
2025-06-16 16:30:58 +0200

    Premier Script

---
+++ script/test.js
@@ -0,0 +1,38 @@
+console.log ("Salut Giga Beau Gosse Que Fait Tu Ici Tu Vien Me fouiller ;)");
+console.log (59);
+console.log (50+10);
+console.log ("50+10");
+
+// Test des Outils Mathématique pour le java script (+) (-) (*) (/) (%) //
+
+console.log (10+10);
+console.log (20-10);
+console.log (10*2);
+console.log (6/2);
+console.log (50%20);
+
+console.log ("70+10 = "+(70+10))
+
+// boolean pour dire vrai ou faux //
+
+console.log (25>50);
+console.log (25<50);
+
+// test //
+
+console.log (3*23);
+
+// crée des variable //
+
+let userAge ="" 
+let firstName ="Vince" ;
+let lastName ="Emery" ;
+
+console.log ("Bonjour " + firstName + " " + lastName + " " );
+
+firstName = "LaMéduse" ;
+lastName = "Roi" ;
+
+console.log (`Salut Mek! ${firstName} ${lastName}`);
+
+// pour faire la différence sur lequel le code va choisir firstname et lastname et juste l'ordre //
---
+++ script/veritable.js
@@ -0,0 +1,24 @@
+let targetNumber = 27
+
+let userNumber = prompt('Devinez un nombre entre 1 et 100 :');
+
+while(userNumber != targetNumber){
+
+if(userNumber > targetNumber) 
+        {alert('C\'est Trop Grand Fumier Réessaie');}
+    else if (userNumber < targetNumber) 
+        {alert('C\'est Trop Petit Chien Réessaie');}
+
+     userNumber = prompt('Devinez un nombre entre 1 et 100 :');
+}
+ {alert('Bien jouer Ma Biche');}
+
+
+
+/*
+let firstName = prompt (`Commennt T'apelle Tu MEK!`) ;
+let lastName = prompt (`Et ton nom de famille MEK!`) ;
+let userAge = prompt (`Combien de temps a tu passé sur Terre ?`)
+
+console.log (`Salut ! ${firstName} ${lastName} tu a ${userAge} ans`);
+*/
