584250162ce97024b088f8e451291cc41014ce29
LaMeduseO <vincepro59@gmail.com>
2025-06-17 11:47:15 +0200

    Intégration jeu dans html-game over à faire

--- script/veritable.js
+++ script/veritable.js
@@ -1,4 +1,36 @@
+let trigger = document.querySelector('#send-button');
+let numberZone = document.querySelector('#user-number');
+let displayZone = document.querySelector('#game-display');
+
 let targetNumber = Math.floor(Math.random()*100)+1;
+
+console.log(trigger, numberZone, displayZone, targetNumber);
+
+function compareNumber(){
+   // console.log("ça marche");
+   let userNumber = numberZone.value;
+   let result;
+   // console.log(userNumber);
+   if (userNumber==targetNumber) {result='gagné'}
+   else if(userNumber > targetNumber) 
+        {result='C\'est Trop Grand Fumier Réessaie';}
+    else if (userNumber < targetNumber) 
+        {result='C\'est Trop Petit Chien Réessaie';}
+
+    let resultZone = document.createElement ('p');
+    resultZone.innerHTML = result;
+    resultZone.style.padding = "5px";
+    resultZone.classList.add('result-line');
+
+    //console.log(resultZone);
+
+    displayZone.appendChild(resultZone);
+
+}
+
+
+trigger.addEventListener('click', compareNumber);
+/* 
 
 console.log (targetNumber);
 
@@ -6,10 +38,7 @@ let userNumber = prompt('Devinez un nombre entre 1 et 100 :');
 
 while(userNumber != targetNumber){
 
-if(userNumber > targetNumber) 
-        {alert('C\'est Trop Grand Fumier Réessaie');}
-    else if (userNumber < targetNumber) 
-        {alert('C\'est Trop Petit Chien Réessaie');}
+
 
      userNumber = prompt('Devinez un nombre entre 1 et 100 :');
 }
