06396142146f6b4e8f9688b2db91b1f135f9bac7
LaMeduseO <vincepro59@gmail.com>
2025-07-29 12:52:02 +0200

    Connect-Box Css ok

--- view/homeView.php
+++ view/homeView.php
@@ -107,20 +107,20 @@
 
             </div>
 
-    <div id="connect-box">
-        <form action="../controller//loginController.php" method="post">
-            <div class="connect-line">
-                <label for="email">email</label>
-                <input type="mail" id="email" name="email" placeholder=" Email " required>
-            </div>
-            <div class="connect-line">
-                <label for="password">Mots De Passe</label>
-                <input type="password" id="password" name="password" placeholder=" Mots De Passe " required>
-            </div>
+    <form id="connect-box" action="../controller/loginController.php" method="post">
+        <div id="connect-close">x</div>
+        <div class="connect-line">
+            <label for="email">Email</label>
+            <input type="mail" id="email" name="email" placeholder="Email" required>
+        </div>
+        <div class="connect-line">
+            <label for="password">Mot de passe</label>
+            <input type="password" id="password" name="password" placeholder="Mot De Passe" required>
+        </div>
+        <div class="connect-line">
+            <div></div>
             <input type="submit" value="Envoyer" class="form-button">
-
-        </form>
-
-    </div>
+        </div>
+    </form>
 </body>
 </html>
